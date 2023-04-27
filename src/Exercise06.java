3c89b143c15c125c3ce03a0e5c9dd858763e34dd
danpham <danhuupham@proton.me>
2023-04-28 01:21:45 +0700

    Add Exercise06

---
+++ src/Exercise06.java
@@ -0,0 +1,62 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Exercise06 {
+    public static void main(String[] args) throws Exception {
+        Job job = Job.getInstance(new Configuration(), "Exercise06");
+        job.setJarByClass(Exercise06.class);
+
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+
+        job.setInputFormatClass(KeyValueTextInputFormat.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        FileSystem fs = FileSystem.get(job.getConfiguration());
+        if (fs.exists(new Path(args[1]))) {
+            fs.delete(new Path(args[1]), true);
+        }
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    public static class Map extends Mapper<Text, Text, Text, Text> {
+        public void map(Text key, Text line, Context context) throws IOException, InterruptedException {
+            StringTokenizer tokenizer = new StringTokenizer(line.toString());
+            while (tokenizer.hasMoreTokens()) {
+                context.write(new Text(tokenizer.nextToken()), key);
+            }
+        }
+    }
+
+    public static class Reduce extends Reducer<Text, Text, Text, IntWritable> {
+        public void reduce(Text word, Iterable<Text> ids, Context context) throws IOException, InterruptedException {
+            HashSet<String> uniqueIds = new HashSet<>();
+            for (Text id : ids) {
+                uniqueIds.add(id.toString());
+            }
+            context.write(word, new IntWritable(uniqueIds.size()));
+        }
+    }
+}
